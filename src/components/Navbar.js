2d323f0a959bba02f1f8c9f10fb9f0f45027e92b
Netson <netsoncavina@gmail.com>
2022-08-01 20:34:07 -0300

    navbar responsiva

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/Navbar.css";
+import ReorderIcon from "@material-ui/icons/Reorder";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [location]);
+
   return (
-    <div className="navbar">
+    <div className="navbar" id={isOpen ? "open" : "close"}>
       <div className="toggleButton">
-        <button></button>
+        <button
+          onClick={() => {
+            setIsOpen((prev) => !prev);
+          }}
+        >
+          <ReorderIcon />
+        </button>
       </div>
       <div className="links">
         <Link to="/">Home</Link>
