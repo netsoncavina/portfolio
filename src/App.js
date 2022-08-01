d03f56a584d72ac26c91c7e94ef674e62c169c77
Netson <netsoncavina@gmail.com>
2022-08-01 20:20:15 -0300

    adicionadas paginas, routers e navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Experiences from "./pages/Experiences";
+import Projects from "./pages/Projects";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="experience" element={<Experiences />} />
+        <Route path="projects" element={<Projects />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="toggleButton">
+        <button></button>
+      </div>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/experience">Experiences</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
