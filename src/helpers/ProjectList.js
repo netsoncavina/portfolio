6eeb69ba5ed12d2130d882410cd375093dcff3aa
Netson <netsoncavina@gmail.com>
2022-08-02 00:21:43 -0300

    adicionada pagina de projetos

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,50 @@
+import projeto1 from "../assets/projeto1.png";
+import projeto2 from "../assets/projeto2.png";
+import projeto3 from "../assets/projeto3.png";
+import projeto4 from "../assets/projeto4.png";
+import projeto5 from "../assets/projeto5.png";
+import projeto6 from "../assets/projeto6.png";
+import projeto7 from "../assets/projeto7.png";
+import projeto8 from "../assets/projeto8.png";
+import projeto9 from "../assets/projeto9.png";
+
+export const projectList = [
+  {
+    name: "League of Legends profile viewer",
+    image: projeto1,
+  },
+  {
+    name: "Trailerflix",
+    image: projeto2,
+  },
+  {
+    name: "Sistema Fatec",
+    image: projeto3,
+  },
+  {
+    name: "Jogo de Plataforma",
+    image: projeto4,
+  },
+  {
+    name: "PlayBay",
+    image: projeto5,
+  },
+
+  {
+    name: "Jogo da memória",
+    image: projeto7,
+  },
+
+  {
+    name: "Alien Run",
+    image: projeto8,
+  },
+  {
+    name: "Tinder LOL",
+    image: projeto9,
+  },
+  {
+    name: "Runeterra Travel Journal",
+    image: projeto6,
+  },
+];
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,3 +1,17 @@
+import ProjectItem from "../components/ProjectItem";
+import "../styles/Projects.css";
+import { projectList } from "../helpers/ProjectList";
 export default function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="projects">
+      <h1> Projetos </h1>
+      <div className="projectList">
+        {projectList.map((project, idx) => {
+          return (
+            <ProjectItem id={idx} name={project.name} image={project.image} />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
