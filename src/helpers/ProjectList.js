29e624c6cf8062ac9fff726e01991f6179d03261
Netson Cavina <netsoncavina@gmail.com>
2023-07-13 16:02:38 -0300

    Adição do campo "categories"

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -17,7 +17,8 @@ export const projectList = [
     description:
       "Rede social focada em conectar jogadores de RPG. O projeto foi desenvolvido utilizando React Native, Expo, Node.js, Express, MongoDB e Socket.io. O Expo foi utilizado para facilitar o desenvolvimento do aplicativo, permitindo a visualização do aplicativo em tempo real no celular. O Socket.io foi utilizado para a interação em tempo real entre os usuários. O MongoDB foi utilizado para armazenar os dados dos usuários. O Node.js e o Express foram utilizados para criar a API REST que comunica o aplicativo com o banco de dados.",
     github: "https://github.com/netsoncavina/HeroisDoRPG-app",
-    // youtube: "https://www.youtube.com/watch?v=qKqfSCmELTo",
+    categories: ["mobile"],
+    youtube: "https://www.youtube.com/watch?v=qKqfSCmELTo",
   },
   {
     name: "Amigos de Patas",
@@ -26,6 +27,7 @@ export const projectList = [
     description:
       "Uma rede social focada em conectar lares temporários ou abrigos para animais de rua com pessoas que queiram adotar um pet. O projeto foi desenvolvido em grupo, utilizando a metodologia ágil Scrum. O backend foi desenvolvido em Node.js, utilizando o Express e o MongoDB. O frontend foi desenvolvido em React, utilizando o Redux para gerenciar o estado da aplicação. O Material-UI foi utilizado para estilização.",
     github: "https://github.com/netsoncavina/AmigosDePatas-frontend",
+    categories: ["web"],
     // vercel: "https://amigosdepatas.vercel.app",
   },
   {
@@ -35,6 +37,7 @@ export const projectList = [
     description:
       "Um profile viewer para League of Legends. Permite visualizar as informações do perfil do jogador, obtendo as informações através de uma API publica da RIOT",
     github: "https://github.com/netsoncavina/lol-profile-viewer",
+    categories: ["web"],
   },
   {
     name: "Trailerflix",
@@ -43,6 +46,7 @@ export const projectList = [
     description:
       "Clone da Netflix, feito para exibir trailers de filmes e séries",
     github: "https://github.com/netsoncavina/TrailerFlix",
+    categories: ["web"],
   },
   {
     name: "Sistema Fatec",
@@ -51,6 +55,7 @@ export const projectList = [
     description:
       "Projeto fullstack para o sistema Fatec. Permite cadastro e consulta de alunos e professores da Fatec",
     github: "https://github.com/netsoncavina/client_faculdade",
+    categories: ["web"],
   },
   {
     name: "Jogo de Plataforma",
@@ -60,6 +65,7 @@ export const projectList = [
       "Jogo de plataforma feito em Python utilizando a biblioteca PyGame. ",
     github: "https://github.com/netsoncavina/Python-Platformer-Game",
     youtube: "YYrog-_0TgM",
+    categories: ["desktop"],
   },
   {
     name: "PlayBay",
@@ -68,6 +74,7 @@ export const projectList = [
     description:
       "E-commerce feito em Java, permite o cadastro de usuários, produtos e pedidos",
     github: "https://github.com/netsoncavina/spring-playbay",
+    categories: ["web"],
   },
 
   {
@@ -77,6 +84,7 @@ export const projectList = [
     description:
       "Jogo da memória utilizando personagens de League of Legends. Possui animações feitas em css e javascript",
     github: "https://github.com/netsoncavina/jogo-da-memoria",
+    categories: ["web"],
   },
 
   {
@@ -87,6 +95,7 @@ export const projectList = [
       "Clone do Chrome Dino feito em Python utilizando a biblioteca PyGame",
     github: "https://github.com/netsoncavina/Chrome-Dinossaur-Clone",
     youtube: "02a58cX08X4",
+    categories: ["desktop"],
   },
   {
     name: "Runeterra Travel Journal",
@@ -96,5 +105,6 @@ export const projectList = [
       "Projeto realizado consumindo a API Data Dragon da RIOT, exibe dados sobre as regiões do jogo, como nomes, imagens, etc. Possui sistema de tradução do texto, podendo ter os textos alterados entre português, inglês e alemão",
     github: "https://github.com/netsoncavina/runeterra_travel_journal",
     // heroku: "https://runeterra-travel-journal.herokuapp.com/",
+    categories: ["web"],
   },
 ];
